a99fa2437f16d57ed537fb221f68e92c7c13c7b3
Tima0102040 <voznesenskiytim@gmail.com>
2023-02-09 13:55:58 +0200

    UrlProperty

--- TestOlx/UnitTest1.cs
+++ TestOlx/UnitTest1.cs
@@ -1,15 +1,30 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Safari;
+
 namespace TestOlx;
 
 public class Tests
 {
+    private IWebDriver driver;
+    private readonly By signInButton = By.XPath("//strong[text()='Ваш профіль']");
     [SetUp]
     public void Setup()
     {
+        driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.olx.ua/uk/");
+        driver.Manage().Window.Maximize();
+        
     }
 
     [Test]
     public void Test1()
     {
         Assert.Pass();
+    }
+
+    [TearDown]
+    public void TearDown1()
+    {
+        
     }
 }
